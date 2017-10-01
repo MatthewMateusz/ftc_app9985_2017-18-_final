12cc62b19c3a5ba45c9db47c28f0723f25df9dbf
Matthew Mateusz <mateuszm@matthewm.com.pl>
2017-10-01 14:48:17 -0500

    Updates worked on 9/27/17
    --Things Added--
    [*] Added Vuforia functions in FTC201718_autonomous
    [*] Added FTC201718_AutoBlue
    [*] Worked on FTC201718_AutoBlue for autonomous
    
     --Things To Add--
     [*] Continue working on vuforia
    
     *^ Items added were not tested
     |^ Items added were tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_AutoBlue.java
@@ -2,8 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
@@ -15,11 +22,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 @Disabled
 @Autonomous (name = "TEMPLATE_Auto")
-public class TEMPLATE_autonomous extends FTC201718_Automation
+public class FTC201718_AutoBlue extends FTC201718_Automation
 {
-
     @Override
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
 
 
@@ -28,17 +34,24 @@ public class TEMPLATE_autonomous extends FTC201718_Automation
         telemetry.update();
 
         //Add processes for Init
+        setupVuforia();
+
+        //We don't know where the robot is, so set it to the orgin
+        //If we don't include this, it would be bnull, which would cause errors later on
+        lastKnownLocation = createMatrix( 0 , 0 , 0 , 0 , 0 , 0);
 
         telemetry.addData("Status" , "Finished Init");
         telemetry.update();
 
         waitForStartAndDisplayWhileWaiting();
-        telemetry.addData("Name" , "Started");
+        telemetry.addData("Status" , "Started");
         telemetry.update();
+
+        visionTargets.activate();
 
         //Add autonomous code here
 
-        telemetry.addData("Name" , "Complete");
+        telemetry.addData("Status" , "Complete");
         telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_Automation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_Automation.java
@@ -3,6 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 /**
  * Created by Matthew on 9/24/2017.
@@ -27,6 +40,22 @@ abstract public class FTC201718_Automation extends LinearOpMode
     public static final double TOUT_SHORT  = 3;
     public static final double TOUT_MEDIUM = 5;
     public static final double TOUT_LONG   = 10;
+
+    //Variables to be used for later
+    public VuforiaLocalizer vuforiaLocalizer;
+    public VuforiaLocalizer.Parameters parameters;
+    public VuforiaTrackables visionTargets;
+    public VuforiaTrackable target;
+    public VuforiaTrackableDefaultListener listener;
+
+    public OpenGLMatrix lastKnownLocation;
+    public OpenGLMatrix phoneLocation;
+
+    private static final String VUFORIA_KEY = "AUYFfMb/////AAAAGeK9R/Mswk3ko4WgwY69fsB3D/KziaC/ZBui6bKvAUjjnhKoPyiDs0+TfVP3vMkYQ4Q0Amo4yosMAH9Xs0k+HX5MHGkhFbGLrDYj5zUN8NinByqcruRQZJuuISEHn1TfD5Fpa9psUmylGexAIwVB6WMfYTL2eKg4EE5mAaRsPgRKZnk/SjMzitYtthDxFusHftOK0N8vywIVSX79mBGmdy6+XUqLLa72zYXUvCrs9lov+xGuC06dUrmpFHl7uwt75QBVb5qyvbsruC4Bfnezzz1S747xiTHQz7Q86q1ZCix2V3AmxQxUuqhlYXDiC6uBseB3npuzuRtNxyCpn6+p3L1qv+Y1axec01BAOUATpSvy";
+
+    private float robotX = 0;
+    private float robotY = 0;
+    private float robotAngle = 0;
 
     //Declare OpMode data
     FTC201718_Actuators_Setup actuators = new FTC201718_Actuators_Setup();
@@ -101,7 +130,46 @@ abstract public class FTC201718_Automation extends LinearOpMode
         actuators.RearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         actuators.RearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    public void setupVuforia()
+    {
+        //Seup parameters to create localizer
+        parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId); // To remove camera view from the scree, remove the R.
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.useExtendedTracking = false;
+        vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
 
+        // These are the vision targets that we want to use
+        // The string needs to be the name of the approprate .xml file in the assets folder
+        visionTargets = vuforiaLocalizer.loadTrackablesFromAsset("RelicVuMark");
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS , 4);
 
+        // Setup the target to be tracked
+        target = visionTargets.get(0); // 0 corresponds to the wheels target
+        target.setName("RelicRecovery"); // target name
+        target.setLocation(createMatrix(0 , 500 , 0 , 90 , 0 , 90));
 
+        //Set the phone location on robot
+        phoneLocation = createMatrix(0 , 255 , 0 , 90 , 0 , 0);
+
+        //Setup listener and inform it of phone information
+        listener = (VuforiaTrackableDefaultListener) target.getListener();
+        listener.setPhoneInformation(phoneLocation , parameters.cameraDirection);
+    }
+
+    //Creates a matrix for determining the locations and orientations of objects
+    //Units are millimeters for x, y, and z, and degrees for u, v, and w
+    public OpenGLMatrix createMatrix (float x , float y , float z , float u , float v , float w)
+    {
+        return OpenGLMatrix.translation(x , y , z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC , AxesOrder.XYZ , AngleUnit.DEGREES , u , v , w
+                ));
+    }
+
+    //Formats a matrix into a readable string
+    public String formatMatrix(OpenGLMatrix matrix)
+    {
+        return matrix.formatAsTransform();
+    }
 }
