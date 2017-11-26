ac1ee1447346c837faae43879e19dbe11c2b7242
Matthew Mateusz <mateuszm@matthewm.com.pl>
2017-11-25 20:30:08 -0600

    --Things Added-- [|] Fixed dumb mistake I made by replace a class (see github past 2-3 version wit AutoBlueLeft & AutoBlueRight) [|] Anded and completed AutoRedRight
     --Things To Add--
     [*] Split auto red into auto red corner and auto red center
    
     *^ Items were not tested
     |^ Items were tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_AutoBlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC201718_AutoBlueLeft.java
@@ -1,6 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.android.dx.dex.code.OutputFinisher;
 
 /**
  * Created by Matthew on 9/27/2017.
@@ -62,38 +77,37 @@ public class FTC201718_AutoBlueLeft extends FTC201718_Automation
         {
             encoderTurnInPlace(SPEED_TURN , 30 , 3);
             ServoArmDown(false);
-            encoderTurnInPlace(SPEED_TURN , -30 , 3);
-            encoderDriveDistance(0.2 , 3 , TOUT_SHORT);
-            encoderTurnInPlace(SPEED_TURN , -90 , TOUT_SHORT);
-            encoderDriveDistance(SPEED_NORMAL , 21 , TOUT_MEDIUM);
-            encoderDriveAside(0.1 , 3.25  + OffSet, TOUT_LONG);
-            encoderDriveDistance(0.2 , 7 , TOUT_LONG);
+            encoderTurnInPlace(SPEED_TURN , 60 , 3);
+            encoderDriveAside(0.3 , -2 , TOUT_SHORT);
+            encoderDriveDistance(SPEED_NORMAL , -30 + OffSet , 3);
+            encoderTurnInPlace( SPEED_TURN , 90 , 3);
+            encoderDriveDistance(0.2 , 9 , 5);
             CylpherGraber(0);
-            encoderDriveDistance(SPEED_NORMAL , -3 , TOUT_SHORT);
+            encoderDriveDistance(0.2 , -6 , 2);
         }
         else if (LeftBallColor == -1) //Left ball is blue
         {
             encoderTurnInPlace(SPEED_TURN , -30 , 3);
             ServoArmDown(false);
-            encoderTurnInPlace(SPEED_TURN , 30 , 3);
-            encoderDriveDistance(0.2 , 3 , TOUT_SHORT);
-            encoderTurnInPlace(SPEED_TURN , -90 , TOUT_SHORT);
-            encoderDriveDistance(SPEED_NORMAL , 21 , TOUT_MEDIUM);
-            encoderDriveAside(0.1 , 3.25  + OffSet, TOUT_LONG);
-            encoderDriveDistance(0.2 , 7 , TOUT_LONG);
+            encoderTurnInPlace(SPEED_TURN , -60 , 3);
+            encoderDriveAside(0.1 , 4 , TOUT_SHORT);
+            encoderDriveDistance(SPEED_NORMAL , 27 + OffSet , 3);
+            encoderTurnInPlace( SPEED_TURN , -90 , 3);
+            encoderDriveDistance(0.2 , 9 , 5);
             CylpherGraber(0);
-            encoderDriveDistance(SPEED_NORMAL , -3 , TOUT_SHORT);
+            encoderDriveDistance(0.2 , -6 , 2);
         }
         else
         {
             telemetry.addData("AUTO: " , "Failed To Detect Color");
             ServoArmDown(false);
-            encoderTurnInPlace(SPEED_TURN , -90 , TOUT_SHORT);
-            encoderDriveDistance(SPEED_NORMAL , 21 , TOUT_MEDIUM);
-            encoderDriveAside(0.1 , 6.5  + OffSet, TOUT_LONG);
-            encoderDriveDistance(0.2 , 7 , TOUT_LONG);
+            encoderDriveDistance(0.1 , 4 , TOUT_SHORT);
+            encoderTurnInPlace(SPEED_TURN , 90 , 3);
+            encoderDriveDistance(SPEED_NORMAL , -29 + OffSet , 3);
+            encoderTurnInPlace( SPEED_TURN , 90 , 3);
+            encoderDriveDistance(0.2 , 10 , 5);
             CylpherGraber(0);
-            encoderDriveDistance(SPEED_NORMAL , -3 , TOUT_SHORT);
+            encoderDriveDistance(0.2 , -6 , 2);
         }
 
 
